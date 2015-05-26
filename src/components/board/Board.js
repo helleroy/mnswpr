107c282642099aa65cd83063de9616229d3b53c5
Henrik Hellerøy <helleroy@gmail.com>
2015-05-26 19:25:18 +0200

    Generating the board with mines

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,36 +1,49 @@
 var React = require('react');
+var _ = require('lodash');
 
 var Tile = require('../tile/Tile');
 
+var boards = {
+    easy: {cols: 8, rows: 8, mines: 8},
+    intermediate: {cols: 16, rows: 16, mines: 40},
+    hard: {cols: 32, rows: 16, mines: 99}
+};
+
 module.exports = React.createClass({
     getDefaultProps: function () {
-        return {size: {width: 16, height: 16}}
+        return {board: boards.intermediate}
     },
     getInitialState: function () {
-        return {tiles: this.generateBoard()};
-    },
-    generateBoard: function () {
-        var tiles = [];
-
-        for (var i = 0; i <= this.props.size.height; i++) {
-            var row = [];
-            for (var j = 0; j <= this.props.size.width; j++) {
-                row.push(<Tile key={this.props.size.height + this.props.size.width}/>)
-            }
-            tiles.push(row);
-        }
-
-        return tiles;
+        return {tiles: generateBoard(this.props.board.rows, this.props.board.cols, this.props.board.mines)};
     },
     render: function () {
         return <table>
             {this.state.tiles.map(function (row) {
                 return <tr>
                     {row.map(function (tile) {
-                        return <td>{tile}</td>
+                        return <td>{tile}</td>;
                     })}
-                </tr>
+                </tr>;
             })}
         </table>;
     }
 });
+
+function generateBoard(rows, cols, numberOfMines) {
+    var length = rows * cols;
+    var mines = [];
+    var tiles = [];
+
+    while (mines.length < numberOfMines) {
+        var random = _.random(0, length - 1);
+        if (!_.includes(mines, random)) {
+            mines.push(random);
+        }
+    }
+
+    for (var i = 0; i < length; i++) {
+        tiles.push(<Tile hasMine={_.includes(mines, i)}/>);
+    }
+
+    return _.chunk(tiles, cols);
+}
--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -7,14 +7,25 @@ module.exports = React.createClass({
             flagged: false
         };
     },
+    onClick: function (event) {
+        if (this.state.revealed) {
+            return;
+        }
+
+        if (event.ctrlKey) {
+            this.setState({flagged: !this.state.flagged})
+        } else if (!this.state.flagged) {
+            this.reveal();
+        }
+    },
     reveal: function () {
-        this.setState({revealed: true});
-    }
-    ,
+        this.setState({revealed: true})
+    },
     render: function () {
         return (
-            <div onClick={this.reveal}>
-                {this.state.revealed ? <div>[]</div> : <div>[X]</div>}
+            <div onClick={this.onClick}>
+                {this.state.revealed ? this.props.hasMine ? <div>[B]</div> : <div>[ ]</div> :
+                    this.state.flagged ? <div>[?]</div> : <div>[X]</div>}
             </div>
         );
     }
